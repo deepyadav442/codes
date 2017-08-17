63b34f5bc69f74e47d36f1dd15b92534fd226f9b
Deepak Yadav <deepyadav442@gmail.com>
2017-08-17 19:00:43 +0530

    Add files via upload

---
+++ simple.cpp
@@ -0,0 +1,82 @@
+
+/*                                  deepyadav442 --> :     c++14     <--                                */
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define ll long long
+/*                                           input                    */
+#define si(i) scanf("%d",&i)
+#define sii(i,j) scanf("%d%d",&i,&j)
+#define siii(i) scanf("%lld",&i)
+#define sss(s) scanf("%s",s)
+/*                                           output                   */
+#define pi(i) printf("%d",i)
+#define pii(i,j) printf("%d%d",i,j)
+#define piii(i) printf("%lld",i)
+#define space printf(" ")
+#define newline printf("\n")
+/*                                          loops &stls                      */
+#define loop(i,s,n) for(i=s;i<n;i++)
+#define looprev(i,s,n) for(i=s;i>=n;i--)
+#define itervec vector<int>::iterator itv
+#define iterrev vector<int>::reverse_iterator it
+#define itermap map<string,int>::iterator itm
+#define pb(S) push_back(S)
+#define mp(i,j) make_pair(i,j)
+#define f first
+#define s second
+#define MAX(a,b) 	(a>b) ? (a) : (b)
+#define MIN(a,b) 	(a<b) ? (a) : (b)
+using namespace std;
+//  fast input output
+inline ll input() //for input integers call it long long int n=input();
+{
+    ll n=0;
+    char ch=getchar_unlocked();
+    while( ch < '0' || ch > '9' )
+    {
+        ch=getchar_unlocked();
+    }
+    while( ch >= '0' && ch <= '9' )
+    {
+        n = (n<<3)+(n<<1) + ch-'0', ch=getchar_unlocked();
+    }
+    return n;
+}
+inline void output(ll n)     //for output call output(count) and then for space use put('\n');
+{
+    char b[35];
+    ll i=0;
+    do
+    {
+        b[i++]=n%10+48;
+        n=n/10;
+    }while(n!=0);
+    --i;
+    while(i>=0)
+    putchar_unlocked(b[i--]);
+}
+inline void faststring(char *str)  //for entering a string use faststring(p);
+{
+    register char c = 0;
+    register long long i = 0;
+    while (c < 33)
+    c = getchar_unlocked();
+    while (c != '\n')
+    {
+        str[i] = c;
+        c = getchar_unlocked();
+        ++i;
+    }
+    str[i] = '\0';
+}
+int main()
+{
+    ll a,A,B;
+    A=input();
+    B=input();
+    a=input();
+    double ans=((double)A)/((double)B);
+    int answer=a/ans;
+    output(answer);
+    return 0;
+}
