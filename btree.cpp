79b39093e0566f530d06680bb2e45b4d6a6f1e04
Deepak <deepyadav442@gmail.com>
2019-09-25 15:03:50 +0530

    More code

---
+++ btree.cpp
@@ -0,0 +1,45 @@
+ /*                                  deepyadav442 --> :     c++14     <--                                */
+#include<bits/stdc++.h>
+#define mod 1000000007
+#define ll long long
+/*                                           input                    */
+#define si(i) scanf("%d",&i)
+#define sii(i,j) scanf("%d%d",&i,&j)
+#define siii(i) scanf("%lld",&i)
+#define sss(s) scanf("%s",s)
+/*                                           output                   */
+#define pi(i) printf("%d",i)
+#define pii(i,j) printf("%d%d",i,j)
+#define piii(i) printf("%lld",i)
+#define space printf(" ")
+#define newline printf("\n")
+/*                                          loops &stls                      */
+#define loop(i,s,n) for(i=s;i<n;i++)
+#define looprev(i,s,n) for(i=s;i>=n;i--)
+#define itervec vector<int>::iterator itv
+#define iterrev vector<int>::reverse_iterator it
+#define itermap map<string,int>::iterator itm
+#define pb(S) push_back(S)
+#define mp(i,j) make_pair(i,j)
+#define f first
+#define s second
+#define MAX(a,b) 	(a>b) ? (a) : (b)
+#define MIN(a,b) 	(a<b) ? (a) : (b)
+using namespace std;
+int cnt=-1;
+typedef struct node{
+	int data;
+	struct node *left,*right;
+}node;
+class BT{
+	public:	
+	BT(int value){
+		this.data=value;
+		left=right=NULL;
+	}
+	node* create(int);
+	void pretraversal(node*);
+};
+
+ 
+	
